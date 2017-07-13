0294b43462161384d39fbc16a407c8c1962e71a6
harshiniavuthu <harshinia544@gmail.com>
2017-07-13 14:11:05 +0530

    implementation of map

---
+++ js/model.js
@@ -0,0 +1,103 @@
+var neighborhood = {
+    locations: [{
+            location: {
+                lat: 50.7374,
+                lng: 7.0982
+            },
+            title: 'Bonn',
+            formatted_address: "Bonn, Germany",
+            place_id: "ChIJSdrLfJ_hvkcRF5ZcaMb424Y",
+        },
+        {
+            location: {
+                lat: 52.5200,
+                lng: 13.4050
+            },
+            title: 'Berlin',
+            formatted_address: "Berlin, Germany",
+            place_id: "ChIJAVkDPzdOqEcRcDteW0YgIQQ",
+        },
+        {
+            location: {
+                lat: 50.1109,
+                lng: 8.6821
+            },
+            title: 'Frankfurt',
+            formatted_address: "Frankfurt, Germany",
+            place_id: "ChIJxZZwR28JvUcRAMawKVBDIgQ",
+        },
+        {
+            location: {
+                lat: 50.9375,
+                lng: 6.9603
+            },
+            title: 'Cologne',
+            formatted_address: "Cologne, Germany",
+            place_id: "ChIJ5S-raZElv0cR8HcqSvxgJwQ",
+        },
+        {
+            location: {
+                lat: 51.0504,
+                lng: 13.7373
+            },
+            title: 'Dresden',
+            formatted_address: "Dresden, Germany",
+            place_id: "ChIJqdYaECnPCUcRsP6IQsuxIQQ",
+        },
+        {
+            location: {
+                lat: 51.3397,
+                lng: 12.3731
+            },
+            title: 'Leipzig',
+            formatted_address: "Leipzig, Germany",
+            place_id: "ChIJcywPIBj4pkcRUvW0udKA35M",
+        },
+        {
+            location: {
+                lat: 49.3988,
+                lng: 8.6724
+            },
+            title: 'Heidelberg',
+            formatted_address: "Heidelberg, Germany",
+            place_id: "ChIJzdzMDgXBl0cR1zokRADq5u8",
+        },
+        {
+            location: {
+                lat: 50.9795,
+                lng: 11.3235
+            },
+            title: 'Weimar',
+            formatted_address: "Weimar, Germany",
+            place_id: "ChIJqVvtMcYapEcRQDYzdMGOIAQ",
+        },
+        {
+            location: {
+                lat: 51.2277,
+                lng: 6.7735
+            },
+            title: 'Dusseldorf',
+            formatted_address: "Dusseldorf, Germany",
+            place_id: "ChIJB1lG8XvJuEcRsHMqSvxgJwQ",
+
+        },
+        {
+            location: {
+                lat: 52.3906,
+                lng: 13.0645
+            },
+            title: 'Potsdam',
+            formatted_address: "Potsdam, Germany",
+            place_id: "ChIJt9Y6hM31qEcRm-yqC5j4ZcU",
+        },
+        {
+            location: {
+                lat: 51.4556432,
+                lng: 7.0115552
+            },
+            title: 'Essen',
+            formatted_address: "Essen, Germany",
+            place_id: "ChIJOfarlrfCuEcRnSytpBHhAGo",
+        }
+    ]
+};
