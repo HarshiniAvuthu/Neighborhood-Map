fd46e991d95da2e892b01b2ffb9cdb283abce9e0
harshiniavuthu <harshinia544@gmail.com>
2017-07-26 21:04:07 +0530

    fix: Infowindow fixes

--- js/model.js
+++ js/model.js
@@ -1,5 +1,6 @@
 var neighborhood = {
     locations: [{
+            id: 1,
             location: {
                 lat: 28.5877,
                 lng: 77.1673
@@ -9,6 +10,7 @@ var neighborhood = {
             place_id: "513a1684e4b0581da224735e",
         },
         {
+            id: 2,
             location: {
                 lat: 28.5212,
                 lng: 77.1790
@@ -18,6 +20,7 @@ var neighborhood = {
             place_id: "563b3462cd109f4faa72ca7a",
         },
         {
+            id: 3,
             location: {
                 lat: 28.4954,
                 lng: 77.0885
@@ -26,16 +29,18 @@ var neighborhood = {
             formatted_address: "Gurgaon",
             place_id: "5257ea5111d20d6aea85a5b6",
         },
-        {
+        {   
+            id: 4,
             location: {
                 lat: 28.60066,
                 lng: 77.2277
             },
-            title: 'he Big Chill Cafe',
+            title: 'The Big Chill Cafe',
             formatted_address: "Saket, New Delhi",
             place_id: "4d17689a6c8b5481d51aeacc",
         },
         {
+            id: 5,
             location: {
                 lat: 28.5534,
                 lng: 77.1942
@@ -45,15 +50,7 @@ var neighborhood = {
             place_id: "4dff63787d8bc001316c3a96",
         },
         {
-            location: {
-                lat: 28.4214,
-                lng: 77.0533
-            },
-            title: 'Good Earth City Centre',
-            formatted_address: "",
-            place_id: "5448a95f498e25e937fa166c",
-        },
-        {
+            id:6,
             location: {
                 lat: 28.5931,
                 lng: 77.2197
--- js/sample.js
+++ js/sample.js
@@ -1,17 +1,16 @@
+var map;
+var infowindow;
+
 // location model to hold data for each location
-var LocationModel = function(loc) {
+var LocationModel = function(loc, markers) {
     var self = this;
     this.location = loc.location;
     this.title = loc.title;
     this.formatted_address = loc.formatted_address;
     this.place_id = loc.place_id;
-
+    this.id = loc.id;
+    this.content = "";
     this.visible = ko.observable(true);
-
-    var infoWindowContent = '<div class="infowindow-scroll">' +
-        '<h3>' + this.title + '</h3>' +
-        '<p>' + this.formatted_address + '</p>' +
-        "Place ID: " + this.place_id + '</div>';
 
     // marker for the location
     this.marker = new google.maps.Marker({
@@ -19,9 +18,26 @@ var LocationModel = function(loc) {
         position: self.location,
         title: self.title,
         animation: google.maps.Animation.DROP,
-        location: self.location,
-        infoWindowContent: infoWindowContent
+        id: self.id,
     });
+
+    this.setContent = function() {
+        if (this.content) {
+            return this.content;
+        }
+        var topTips = [];
+        var venueUrl = 'https://api.foursquare.com/v2/venues/' + this.place_id + '/tips?sort=recent&limit=5&v=20150609&client_id=GJGKB305BEZUYLZXVUPBLWHE5UD4WRXH2SV54BDXX412QGHT&client_secret=MH0A5CAVN14LFE3NNXJ211S4UQRA2LNDVDQM4A2HMQCVDPMZ';
+        var that = this;
+
+        $.getJSON(venueUrl, function(data) {
+            data.response.tips.items.forEach(function(item) {
+                topTips.push('<li>' + item.text + '</li>');
+            });
+        }).done(function() {
+            that.content = '<h3>' + that.title + '</h3> <h4> Top tips: </h4>' + '<ol class="tips">' + topTips.join('') + '</ol>';
+        });
+    };
+
     // if a location is visible, show the corresponding marker on map otherwise hide the marker
     this.showMarker = ko.computed(function() {
         if (self.visible() === true) {
@@ -30,42 +46,42 @@ var LocationModel = function(loc) {
             self.marker.setVisible(false);
         }
     });
+
+    // function that opens infowindow when clicked on that particular marker.
+    google.maps.event.addListener( this.marker, 'click', function() {
+        markers.forEach( function(marker) {
+            if (marker.marker.getAnimation() !== null) {
+                marker.marker.setAnimation(null);
+            }
+        });
+        this.setAnimation(google.maps.Animation.BOUNCE);
+        infowindow.setContent('<div>' + markers[this.id - 1].content + '</div>');
+        infowindow.open(map, this);
+    });
 };
 
 // viewModel provides data binding using knockout
-var map;
 var ViewModel = function() {
     var self = this;
     var bounds = new google.maps.LatLngBounds();
+    infowindow = new google.maps.InfoWindow();
     // new google map
     map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 7,
+        zoom: 10,
         center: {
-            lat: 51.1657,
-            lng: 10.4515
+            lat: 28.7041,
+            lng: 77.1025
         }
     });
+
     // read locations and push into observableArray
     this.locations = ko.observableArray();
     neighborhood.locations.forEach(function(location) {
-        self.locations.push(new LocationModel(location));
-    });
-    this.currentLocation = ko.observable(this.locations()[0]);
-    
-    this.infoWindow = new google.maps.InfoWindow(); // creating multiple 
-
-    this.locations().forEach(function(location) {
-        var marker = location.marker;
-        if (self.infoWindow.marker != marker) {
-            self.infoWindow.marker = marker;
-            marker.addListener('click', function() {
-                self.currentLocation(location);
-                toggleBounce(this);
-                populateInfoWindow(this);
-            });
-        }
-        bounds.extend(marker.position);
-    });
+        var myMarker = new LocationModel(location, self.locations());
+        myMarker.setContent();
+        self.locations.push(myMarker);
+         bounds.extend(myMarker.marker.position);
+    });    
 
     google.maps.event.addDomListener(window, 'resize', function() {
         map.fitBounds(bounds);
@@ -92,15 +108,13 @@ var ViewModel = function() {
 
     // function to handle when a list item is clicked
     this.listClick = function(clickedItem) {
-        // update currentLocation when list item is clicked
-        self.currentLocation(clickedItem);
         var marker = clickedItem.marker;
         map.setCenter(marker.getPosition());
         toggleBounce(marker);
         populateInfoWindow(marker);
     };
     // hide modal when close-button is clicked
-    this.modalVisible = ko.observable(false);
+    this.modelVisible = ko.observable(false);
     this.exitModal = function() {
         self.modalVisible(false);
     };
@@ -111,11 +125,11 @@ var ViewModel = function() {
             marker.setAnimation(null);
         }, 1400);
     }
+
     // attach marker to infoWindow and setting content and click event from infowindow
     function populateInfoWindow(marker) {
-        self.infoWindow.marker = marker;
-        self.infoWindow.setContent(marker.infoWindowContent);
-        self.infoWindow.open(map, marker);
+        infowindow.setContent('<div>' + self.locations()[marker.id - 1].content + '</div>');
+        infowindow.open(map, marker);
     }
     // handle button click event inside infowindow
     this.showModal = function() {
@@ -146,7 +160,6 @@ $(".hamburger").click(function() {
         $(".hamburger").hide();
     });
 });
-
 
 var mapError = function() {
     document.getElementById('map-error').style.display = 'block';
